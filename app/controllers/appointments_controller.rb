dca306d92868cbf1d7d657bec6b39614ae891027
George Lin <george@zearn.com>
2017-10-11 23:25:24 -0400

    Add ability to add and view new appointments, move appointments list into partial

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,4 +3,15 @@ class AppointmentsController < ApplicationController
     @appointments = Appointment.order('appt_time ASC')
     @appointment = Appointment.new
   end
+
+  def create
+    @appointment = Appointment.create(appointment_params)
+    @appointments = Appointment.order('appt_time ASC')
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:title, :appt_time)
+  end
 end
