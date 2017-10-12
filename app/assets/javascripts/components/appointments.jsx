5010cfbad74474acb7c3a578b9e3f0cb7539f5e7
George Lin <george@zearn.com>
2017-10-11 23:49:52 -0400

    Add input title and appt_time to state

--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -1,8 +1,18 @@
 var Appointments = React.createClass({
+  getInitialState: function() {
+    return {
+      appointments: this.props.appointments,
+      input_title: 'Team standup meeting',
+      input_appt_time: 'Tomorrow at 9am'
+    }
+  },
+
   render: function() {
     return (
       <div>
-        <AppointmentForm />
+        <AppointmentForm
+          input_title={this.state.input_title}
+          input_appt_time={this.state.input_appt_time} />
         <AppointmentsList appointments={this.props.appointments} />
       </div>
     )
