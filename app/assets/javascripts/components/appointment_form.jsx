cdf1c009285979546a1c73e7286aedb9879fe5cd
George Lin <george@zearn.com>
2017-10-12 00:08:37 -0400

    Track state of title and appt_time in form

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -1,11 +1,22 @@
 var AppointmentForm = React.createClass({
+  handleChange: function(e) {
+    var name = e.target.name;
+    obj = {};
+    obj[name] = e.target.value;
+    this.props.onUserInput(obj);
+  },
+
   render: function() {
     return (
       <div>
         <h2>Make a new appointment </h2>
         <form>
-          <input name='title' placeholder='Appointment Title' value={this.props.input_title} />
-          <input name='appt_time' placeholder='Date and time' value={this.props.input_appt_time} />
+          <input name='title' placeholder='Appointment Title'
+            value={this.props.title}
+            onChange={this.handleChange} />
+          <input name='appt_time' placeholder='Date and time'
+            value={this.props.appt_time}
+            onChange={this.handleChange} />
           <input type='submit' value='Make Appointment' />
         </form>
       </div>
--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -2,17 +2,22 @@ var Appointments = React.createClass({
   getInitialState: function() {
     return {
       appointments: this.props.appointments,
-      input_title: 'Team standup meeting',
-      input_appt_time: 'Tomorrow at 9am'
+      title: 'Team standup meeting',
+      appt_time: 'Tomorrow at 9am'
     }
+  },
+
+  handleUserInput: function(obj) {
+    this.setState(obj);
   },
 
   render: function() {
     return (
       <div>
         <AppointmentForm
-          input_title={this.state.input_title}
-          input_appt_time={this.state.input_appt_time} />
+          title={this.state.title}
+          appt_time={this.state.appt_time}
+          onUserInput={this.handleUserInput} />
         <AppointmentsList appointments={this.props.appointments} />
       </div>
     )
